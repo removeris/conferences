2266457c00f32d3785144a26baa3a37c1647d60f
removeris <108592864+removeris@users.noreply.github.com>
2025-12-04 13:37:04 +0200

    add home/about page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,24 @@
-{{-- resources/views/conferences/index.blade.php --}}
 @extends('layout.base')
 
-@section('title', 'Conferences | Home')
+<head>
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+</head>
+
+@section('title', 'Conferences | About')
 
 @section('content')
 
-    <h2>Home</h2>
+    <h1>About This Project</h1>
+
+    <p>
+        This platform is a simple conference and event publishing system.
+        It allows administrators to create, edit, and manage technical
+        conferences and public events in a clean and accessible way.
+    </p>
+
+    <p>
+        Visitors can browse upcoming conferences, view event details,
+        and stay informed about what is happening next.
+    </p>
 
 @endsection
